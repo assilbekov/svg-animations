52fb92c23f15e608eb288b0f4ccab86321e7aa86
assilbekov <askhat.assilbekov@gmail.com>
2024-03-26 03:49:08 -0300

    App finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { CheckIcon, CircleStackIcon } from "@heroicons/react/24/outline";
+import Flame from "./components/Flame";
+import { useRef, useState } from "react";
 
 export default function App() {
+  const cardsRef = useRef<HTMLElement>(null)
+  const [cursor, setCursor] = useState({ x: 0, y: 0 })
+  const [mouseOnCard, setMouseOnCard] = useState(false)
+
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLElement, MouseEvent>
+  ) => {
+    if (cardsRef.current !== null) {
+      const rect = cardsRef.current.getBoundingClientRect()
+      const x = event.clientX - rect.left
+      const y = event.clientY - rect.top
+      setCursor({ x: x, y: y })
+    }
+  }
+
   return (
     <main className="w-full h-screen flex place-items-center justify-center">
-      <section className="card">
+      <section
+        className="card"
+        ref={cardsRef}
+        onMouseEnter={() => setMouseOnCard(true)}
+        onMouseLeave={() => setMouseOnCard(false)}
+        onMouseMove={(event) => handleMouseMove(event)}
+      >
         <div className="flex flex-col w-2/5 justify-between">
           <div className="flex flex-col gap-5">
             <CircleStackIcon className="w-14 rounded-lg bg-neutral-950/70 stroke-emerald-500 p-2 shadow-inner" />
@@ -27,6 +50,10 @@ export default function App() {
               <p>Easy to extend</p>
             </span>
           </div>
+        </div>
+        <div className="w-3/5 flex flex-col place-items-center">
+          {/* SVG Here */}
+          <Flame cursor={cursor} cardRef={cardsRef} mouseOnCard={mouseOnCard} />
         </div>
       </section>
     </main>
